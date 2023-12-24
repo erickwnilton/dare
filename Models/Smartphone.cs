318cf8fd4912f922269ce0abf5a7db9e00ed033c
23eerick <erickwelberth1@hotmail.com>
2023-12-24 13:26:01 -0300

    finished project

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,12 +3,32 @@ namespace dare.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo {get; set; }
+        private string IMEI {get; set; }
+        private int Memoria {get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
+
+        }
+
+        public string GetModelo()
+        {
+            return Modelo;
+        }
+
+        public string GetIMEI()
+        {
+            return IMEI;
+        }
+
+        public int GetMemoria()
+        {
+            return Memoria;
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using dare.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Nokia");
+Smartphone nokia = new Nokia(numero: "", modelo: "", imei: "", memoria: 1);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+nokia.ReceberLigacao();
+
+Console.WriteLine("IPhone");
+Smartphone iphone = new Iphone(numero: "", modelo: "", imei: "", memoria: 1);
+iphone.Ligar();
+iphone.InstalarAplicativo("Instagram");
+iphone.ReceberLigacao();
